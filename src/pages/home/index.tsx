4b8ded9f9ca4ef65601bb8c905800cfd075a52a8
Daniel Costa Carvalho Martins <danielcostacarvalhomartins06@gmail.com>
2026-02-04 15:57:35 -0300

    consumodeapi

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,57 @@
 import styles from './home.module.css';
 import { BsSearch } from 'react-icons/bs';
 import { Link, useNavigate } from 'react-router-dom';
-import { useState, type FormEvent } from 'react';
+import { useState, type FormEvent, useEffect } from 'react';
+
+interface CoinProps {
+    id: string;
+    name: string;
+    symbol: string;
+    pricdeUsd: string;
+    marketCapUsd: string;
+    rank: string;
+    changePercent24Hr: string;
+    supply: string;
+    maxSupply: string;
+    vwap24Hr: string;
+    volumeUsd24Hr: string;
+    explorer: string;
+}
+
+interface DataProps {
+    data: CoinProps[];
+}
 
 export function HomePage() {
 
     const [input, setInput] = useState('');
+    const [coins, setCoins] = useState<CoinProps[]>([]);
+
+    useEffect(() => {
+        getData();
+    }, []);
+
+    async function getData() {
+        fetch("https://rest.coincap.io/v3/assets?limit=10&offset=0&apiKey=04a07e84652a8216b58b05be9a2b603a7201ec7db445a28ce65f7c1796503d36")
+        .then((response) => response.json())
+        .then((data: DataProps) => {
+            const coinsData = data.data;
+            const price = Intl.NumberFormat('en-US', {
+                style: 'currency',
+                currency: 'USD',
+            });
+            const formatedCoins = coinsData.map((coin) => {
+                const formated = {
+                    ...coin,
+                    formatedPrice: price.format(Number(coin.pricdeUsd))
+                }
+
+                return formated;
+            })
+
+            console.log(formatedCoins);
+        })
+    }
 
     const navigate = useNavigate();
 
